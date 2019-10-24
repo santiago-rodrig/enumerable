173978d6e125f23f585544c13daf8d5a94564abd
Santiago <santo1996.29@gmail.com>
2019-10-24 09:19:19 -0400

    more refinement; my_all? completed

--- enumerable.rb
+++ enumerable.rb
@@ -3,24 +3,32 @@
 # This is my own implementation of the Enumerable module methods
 module Enumerable
   def my_each
-    size.times { |i| yield to_a[i][0], to_a[i][1] } if is_a? Hash
-    size.times { |i| yield to_a[i] } unless is_a? Hash
+    as_array = to_a
+    size.times { |i| yield as_array[i][0], as_array[i][1] } if is_a? Hash
+    size.times { |i| yield as_array[i] } unless is_a? Hash
     self
   end
 
   def my_each_with_index
-    size.times { |i| yield to_a[i], i }
+    as_array = to_a
+    size.times { |i| yield as_array[i], i }
     self
   end
 
   def my_select
     if is_a? Hash
       selection = {}
-      my_each { |key, value| selection[key] = value if yield key, value }
+      my_each { |k, v| selection[k] = v if yield k, v }
       return selection
     end
     selection = []
-    size.times { |i| selection << to_a[i] if yield to_a[i] }
+    my_each { |i| selection << i if yield i }
     selection
+  end
+
+  def my_all?
+    my_each { |k, v| return false unless yield k, v } if is_a? Hash
+    my_each { |i| return false unless yield i } unless is_a? Hash
+    true
   end
 end
