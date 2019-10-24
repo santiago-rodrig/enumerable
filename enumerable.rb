391af518e4af7e1a6f325ffef9f771916f4a4fe9
Santiago <santo1996.29@gmail.com>
2019-10-24 11:26:08 -0400

    conditional enumerator return

--- enumerable.rb
+++ enumerable.rb
@@ -3,6 +3,8 @@
 # This is my own implementation of the Enumerable module methods
 module Enumerable
   def my_each
+    return to_enum :my_each unless block_given?
+
     as_array = to_a
     size.times { |i| yield as_array[i][0], as_array[i][1] } if is_a? Hash
     size.times { |i| yield as_array[i] } unless is_a? Hash
@@ -10,12 +12,16 @@ module Enumerable
   end
 
   def my_each_with_index
+    return to_enum :my_each_with_index unless block_given?
+
     as_array = to_a
     size.times { |i| yield as_array[i], i }
     self
   end
 
   def my_select
+    return to_enum :my_select unless block_given?
+
     if is_a? Hash
       selection = {}
       my_each { |k, v| selection[k] = v if yield k, v }
