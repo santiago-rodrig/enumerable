0a2e94e7d74cf214c0d4e9075a9c20ca1eaa44fe
Mohamed Naser <nc_m.naser@hotmail.com>
2019-11-06 19:36:03 +0200

    finish my_each test cases

--- spec/enumerable_loops_spec.rb
+++ spec/enumerable_loops_spec.rb
@@ -13,7 +13,7 @@ describe Enumerable do
 
     let(:array) { [1, 2, 3, 'bob', 'mohamed', :santiago, false] }
     let(:hash) { { name: 'susan', job: 'cooker', age: 35, 1 => [1, 2, false] } }
-    let(:range) { (1..78) } 
+    let(:range) { (1..78) }
 
     # looping methods shouldn't receive arguments
     # so this can be summarized
@@ -28,8 +28,8 @@ describe Enumerable do
       describe '#my_each_with_index' do
         it 'should raise ArgumentError' do
           expect { array.my_each_with_index(1, 2, false) }.to raise_error(ArgumentError)
-          expect { array.my_each_with_index(1, 2, false) }.to raise_error(ArgumentError)
-          expect { array.my_each_with_index(1, 2, false) }.to raise_error(ArgumentError)
+          expect { hash.my_each_with_index(1, 2, false) }.to raise_error(ArgumentError)
+          expect { range.my_each_with_index(1, 2, false) }.to raise_error(ArgumentError)
         end
       end
     end
@@ -37,10 +37,16 @@ describe Enumerable do
     context 'without block' do
       describe '#my_each' do
         it 'should return an enumerable' do
+          expect(array.my_each.is_a?(Enumerable)).to be_truthy
+          expect(hash.my_each.is_a?(Enumerable)).to be_truthy
+          expect(range.my_each.is_a?(Enumerable)).to be_truthy
         end
       end
       describe '#my_each_with_index' do
         it 'should return an enumerable' do
+          expect(array.my_each_with_index.is_a?(Enumerable)).to be_truthy
+          expect(hash.my_each_with_index.is_a?(Enumerable)).to be_truthy
+          expect(range.my_each_with_index.is_a?(Enumerable)).to be_truthy
         end
       end
     end
@@ -49,10 +55,34 @@ describe Enumerable do
       context 'with no variables' do
         describe '#my_each' do
           it 'should iterate without yielding anything' do
+            counter = 0
+            array.my_each { counter += 1 }
+            expect(counter).to eq(array.size)
+
+            # reset counter
+            counter = 0
+            hash.my_each { counter += 1 }
+            expect(counter).to eq(hash.size)
+            # reset counter
+            counter = 0
+            range.my_each { counter += 1 }
+            expect(counter).to eq(range.size)
           end
         end
         describe '#my_each_with_index' do
           it 'should iterate without yielding anything' do
+            counter = 0
+            array.my_each_with_index { counter += 1 }
+            expect(counter).to eq(array.size)
+
+            # reset counter
+            counter = 0
+            hash.my_each_with_index { counter += 1 }
+            expect(counter).to eq(hash.size)
+            # reset counter
+            counter = 0
+            range.my_each_with_index { counter += 1 }
+            expect(counter).to eq(range.size)
           end
         end
       end
@@ -60,20 +90,54 @@ describe Enumerable do
         context 'arrays and ranges' do
           describe '#my_each' do
             it 'should iterate yielding values' do
+              temp_array = []
+              array.my_each { |element| temp_array << element }
+              expect(temp_array).to eq(array)
+
+              # reset the temp array
+              temp_array = []
+              range.my_each { |element| temp_array << element }
+              expect(temp_array).to eq(range.to_a)
             end
           end
           describe '#my_each_with_index' do
             it 'should iterate yielding values' do
+              temp_array = []
+              array.my_each_with_index { |element| temp_array << element }
+              expect(temp_array).to eq(array)
+
+              # reset the temp array
+              temp_array = []
+              range.my_each_with_index { |element| temp_array << element }
+              expect(temp_array).to eq(range.to_a)
             end
           end
         end
         context 'hashes' do
           describe '#my_each' do
             it 'should iterate yielding key-value pairs as arrays' do
+              temp_keys_array = temp_values_array = []
+
+              hash.my_each do |row|
+                temp_keys_array << row[0]
+                temp_values_array << row[1]
+              end
+
+              expect(temp_keys_array).to eql(hash.keys)
+              expect(temp_values_array).to eql(hash.values)
             end
           end
           describe '#my_each_with_index' do
-            it 'should iterate yielding key-value pairs as arrays and the index of the hash as an array' do
+            it 'should iterate yielding key-value pairs as arrays ' do
+              temp_keys_array = temp_values_array = []
+
+              hash.my_each_with_index do |row|
+                temp_keys_array << row[0]
+                temp_values_array << row[1]
+              end
+
+              expect(temp_keys_array).to eql(hash.keys)
+              expect(temp_values_array).to eql(hash.values)
             end
           end
         end
@@ -82,6 +146,14 @@ describe Enumerable do
         context 'arrays and ranges' do
           describe '#my_each' do
             it 'should iterate as with 1 variable' do
+              temp_keys_array = []
+
+              array.my_each do |key, _value|
+                temp_keys_array << key
+              end
+
+              expect(temp_keys_array).to eql(array)
+              # expect(temp_values_array).to eql(hash.values)
             end
           end
           describe '#my_each_with_index' do
