9b1bbc8d74c326a9a702c34105368dd2f9f9ebc0
Santiago <santo1996.29@gmail.com>
2019-11-06 14:57:46 -0400

    Added test files
    test files created:
    - enumerable_conditionals_spec.rb
    - enumerable_misc_spec.rb

---
+++ spec/enumerable_misc_spec.rb
@@ -0,0 +1,20 @@
+# froze_string_literal: true
+
+require_relative '../enumerable.rb'
+
+describe Enumerable do
+  context 'with arguments' do
+  end
+  context 'without a block' do
+  end
+  context 'with a block' do
+    context 'without variables' do
+    end
+    context 'with 1 variable' do
+    end
+    context 'with 2 variables' do
+    end
+    context 'with more than 2 variables' do
+    end
+  end
+end
