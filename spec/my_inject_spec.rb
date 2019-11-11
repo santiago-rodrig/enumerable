a544fb02db37af0ef32bbfa31ea63ace242f751c
Santiago <santo1996.29@gmail.com>
2019-11-11 13:34:57 -0400

    template for my_inject

---
+++ spec/my_inject_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require_relative '../enumerable'
+
+# TODO
+describe '#my_inject' do
+  context 'with one argument and no block' do
+  end
+
+  context 'with two arguments and no block' do
+  end
+
+  context 'with one argument and a block of 0 variables' do
+  end
+
+  context 'with one argument and a block of 1 variable' do
+  end
+
+  context 'with one argument and a block of 2 variables' do
+  end
+
+  context 'with one argument and a block of 3 variables' do
+  end
+
+  context 'with no arguments and a block of 0 variables' do
+  end
+
+  context 'with no arguments and a block of 1 variable' do
+  end
+
+  context 'with no arguments and a block of 2 variables' do
+  end
+
+  context 'with no arguments and a block of 3 variables' do
+  end
+end
